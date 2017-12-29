804d152135d5248e2b58433d4b8607c3c2f827ca
leon3s <lvernisse@student.42.fr>
2017-12-29 22:08:16 +0100

    first commit

---
+++ CreateData.js
@@ -0,0 +1,38 @@
+'use strict';
+
+const server = require('./server/server');
+
+const {Member} = server.models;
+
+// Member.create([
+//   {
+//     firstname: 'leone',
+//     gender: 'male',
+//   },
+//   {
+//     firstname: 'toto',
+//     gender: 'male',
+//   },
+//   {
+//     firstname: 'xoox',
+//     gender: 'male',
+//   },
+//   {
+//     firstname: 'tinia',
+//     gender: 'female',
+//   },
+//   {
+//     firstname: 'cindy',
+//     gender: 'female',
+//   },
+//   {
+//     firstname: 'xodsa',
+//     gender: 'male',
+//   },
+//   {
+//     firstname: 'dasdas',
+//     gender: 'male',
+//   },
+// ], (err, members) => {
+//   console.log('Member');
+// });
